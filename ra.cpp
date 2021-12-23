f2fceb23e6818b57b30131f7ba10133d40900c25
pouria <s.pouria.h2003@gmail.com>
2021-12-23 15:28:37 +0330

    just learning git :)

--- ra.cpp
+++ ra.cpp
@@ -1,39 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <Windows.h>
+
 int main()
 {
-    char word[100];
-    scanf ("%s",word);
-    int length=strlen(word);
-    int last_char=length;
-    int result=0;
-    int counter=0;
-    for (int i=0;i<=last_char-1;i++)
-    {
-        if (word[last_char-1]==word[i])
-        {
-            int copy=last_char-1;
-            for (int j=i;j<copy;)
-            {
-                copy--;
-                j++;
-                if (word[copy]!=word[j])
-                {
-                    counter=0;
-                    break;
-                }
-                else if (word[copy]==word[j]) counter+=2;
-            }
-            
-        } if (counter>0) 
-        {
-            counter+=((last_char-1)-i+1)%2;
-            break;
-        }
-    }
-    if (counter==0) counter++;
-    if (counter>length) counter=length;
-    result=length-counter;
-    printf ("%d",result);
+	system("color 2");
+    printf("hello world");
+    Sleep(1000);
+    system("cls");
+    system("color 5");
+    printf("we are learning github :)");
+    Sleep(1000);
+    system("cls");
+    system("color 0");
     return 0;
 }
